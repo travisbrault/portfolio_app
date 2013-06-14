19b0a19ed1a3ab7c40314a34ad9c9643b54666d9
travisbrault <brault.travis@gmail.com>
2013-06-14 15:13:11 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 PortfolioApp::Application.routes.draw do
+
+get "static_pages/home"
+get "static_pages/about"
+get "static_pages/contact"
+get "static_pages/portfolio"
+  
+  #root to: 'static_pages#home'
+
+  #match '/portfolio',    to: 'static_pages#portfolio'
+  #match '/about',        to: 'static_pages#about'
+  #match '/contact',      to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
